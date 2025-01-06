a8fcc65ab802caa9758ecf7a44b1db4aecd1239e
Sky Chew <sky@skychew.com>
2025-01-06 20:11:09 +0800

    added Debug back

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::sync::Arc;
 use tokio;
 use dotenv::dotenv;
 //use log::{info, debug, error};
-use log::{info, error};
+use log::{info, debug, error};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
