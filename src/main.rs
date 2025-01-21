9ffeee2a3459bf94c29773fe9108f081b95a02f7
Sky Chew <sky@skychew.com>
2025-01-21 23:01:35 +0800

    clean up /// commenting warning

--- src/main.rs
+++ src/main.rs
@@ -138,8 +138,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         info!("💰 Amount In: {:?}", amount_in);
                         info!("👤 Recipient: {:?}", recipient);
 
-                        /// Fetch prices from DEXs
-
                         // Router Addresses
                         let sushi_router: Address = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F".parse()?;
                         let uniswap_router: Address = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D".parse()?;
