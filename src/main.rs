1255ed23d82fbfea36fe62b5814099a494c19eb7
Sky Chew <sky@skychew.com>
2025-01-24 08:42:21 +0800

    remove premature quitting

--- src/main.rs
+++ src/main.rs
@@ -212,8 +212,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                             // === Simulate Arbitrage ===
                             simulate_arbitrage(sushi_price, uniswap_price, amount_in)?;
                         }else {
-                            println!("❌ Skipping...");
-                            return Ok(()); // Skip further processing
+                            warn!("❌ Skipping...");
                         }
                     }
                 }
