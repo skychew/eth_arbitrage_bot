0dd2d9598baec1bca80e5e5d1cf4e479c5384d34
Sky Chew <sky@skychew.com>
2025-01-31 10:54:10 +0800

    fix

--- src/bin/manualCheckHash.rs
+++ src/bin/manualCheckHash.rs
@@ -143,7 +143,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap();
 
         println!("Tx Hash: {:?}, Hash#: {}",tx_hash,hash_count); 
-        hash_count += 1; 
+        //hash_count += 1; 
         //Tx only counts fetch_transaction and fetch_price
  /*
         print!("\rHash#: {} | Review#: {} | Abtrg#: {} | Tx#: {} | Fail#: {} | 1stTry#: {} | Retry#: {} | RtryErr#: {} | isMined#: {}", 
