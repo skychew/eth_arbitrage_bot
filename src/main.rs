02d6796ad09aa2c0bad3c57f87b6cae15f55aec8
Sky Chew <sky@skychew.com>
2025-02-10 11:54:00 +0800

    fix matching_Address

--- src/main.rs
+++ src/main.rs
@@ -170,15 +170,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             ===========
             AMT ETH will be 0 if no ethereum transffered. But there will sometimes be value even if the transfer tokens doest not have Eth. It could be payments for the gas fee.
         */
-        let mut arbitrage_detected = false;
-        let mut detected_dex_name = String::new();
-        let mut matching_address = None;
+        
         if let Some(transaction) = fetch_transaction(provider.clone(), tx_hash, rate_limiter.clone()).await {
             REVIEW_COUNT.fetch_add(1, Ordering::SeqCst);
 
             if let Some(to) = transaction.to {
                 
-                if let Some((detected_dex_name, _)) = dex_groups.iter().find(|(_, addresses)| {
+                if let Some((detected_dex_name, matching_address)) = dex_groups.iter().find(|(_, addresses)| {
                     addresses.iter().any(|(address, _)| address == &to)
                 }) {
                     TODEX_COUNT.fetch_add(1, Ordering::SeqCst);
