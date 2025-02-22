723f67b58cde87cead38116343fe7c44efc21b74
Sky Chew <sky@skychew.com>
2025-02-22 15:40:09 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -167,10 +167,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     debug!("📡 Fetching valid trading pairs from Binance...");
     let valid_pairs = fetch_valid_pairs().await?;
 
-    while let Some(tx_hash_result) = stream.next().await {
+    while let Some(tx_hash) = stream.next().await {
         //check if we run out of infura credits
-        match tx_hash_result {
-            Ok(tx_hash) => {
+        match let Some(tx_hash) {
+            Ok let Some(tx_hash) => {
                 debug!("Tx Hash: {:?}",tx_hash); 
                 hash_count += 1; 
                 //Tx only counts fetch_transaction and fetch_price
